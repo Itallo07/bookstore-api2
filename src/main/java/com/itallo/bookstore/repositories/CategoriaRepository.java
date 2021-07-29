e81e1583c1db4fef23582b618b00d893243215c8
itallo07 <itallodanillo@hotmail.com>
2021-07-29 15:24:02 -0300

    instancia da base de dados

---
+++ src/main/java/com/itallo/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.itallo.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.itallo.bookstore.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
